1b8b06c729e70f7e5d10071b14df17ce4fde77ab
João Claudio <jcdias1141@gmail.com>
2021-12-09 17:46:36 -0300

    Dicionario ADD

--- array/js/main.js
+++ array/js/main.js
@@ -1,34 +1,46 @@
 //Array
-const lista = ["Maça", "Pera", "Banana"]
-//listar a posição 0
-console.log(lista[0]);
+// const lista = ["Maça", "Pera", "Banana"]
+// //listar a posição 0
+// console.log(lista[0]);
 
-//listar todos os itens da lista 
-console.log(lista)
+// //listar todos os itens da lista 
+// console.log(lista)
 
-//adicionando uma nova fruta
-lista.push("Uva");
+// //adicionando uma nova fruta
+// lista.push("Uva");
 
-//listar todos os itens da lista + novo item 
-console.log(lista)
+// //listar todos os itens da lista + novo item 
+// console.log(lista)
 
-//comando pop para remover o ultimo item da lista
-lista.pop();
+// //comando pop para remover o ultimo item da lista
+// lista.pop();
 
-console.log(lista)
+// console.log(lista)
 
-//verificar tamanho da lista
-console.log(lista.length);
+// //verificar tamanho da lista
+// console.log(lista.length);
 
-//reordenar itens da lista
-console.log(lista.reverse());
+// //reordenar itens da lista
+// console.log(lista.reverse());
 
-//retornar como String
-console.log(lista.toString());
+// //retornar como String
+// console.log(lista.toString());
 
-//adiciona elemento para separar os itens
+// //adiciona elemento para separar os itens
 
-console.log(lista.join(" - "))
+// console.log(lista.join(" - "))
+
+//Dicionario
+
+var frutas = [{
+    nome: "Maça",
+    cor: "vermelha"    
+}, {
+    nome: "Abacaxi",
+    cor: "Amarelo"
+}]
+
+console.log(frutas);
 
 
 
