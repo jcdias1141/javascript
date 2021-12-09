7c91acec62ff9096f4e5d5bf3c5aff418e8575cf
João Claudio <jcdias1141@gmail.com>
2021-12-09 17:12:09 -0300

    Aula-01

---
+++ introducao/js/main.js
@@ -0,0 +1,13 @@
+let nome = "Github";
+let idade = 27;
+var frase = "Japão é melhor time do mundo!!";
+//alert("Seja bem vindo: " + nome + " ,verificamos que a sua idade é: " + idade);
+
+console.log(nome)
+console.log(idade)
+
+//replace altera o japão para Brasil
+console.log(frase.replace("Japão", "Brasil"))
+
+//toUpperCase deixa as letras em maiusculo
+console.log(frase.toUpperCase());
