89886321b7a7ee3dda91608ccdd05550fd7044c9
João Claudio <jcdias1141@gmail.com>
2021-12-09 17:36:06 -0300

    ADD Array

---
+++ array/js/main.js
@@ -0,0 +1,36 @@
+//Array
+const lista = ["Maça", "Pera", "Banana"]
+//listar a posição 0
+console.log(lista[0]);
+
+//listar todos os itens da lista 
+console.log(lista)
+
+//adicionando uma nova fruta
+lista.push("Uva");
+
+//listar todos os itens da lista + novo item 
+console.log(lista)
+
+//comando pop para remover o ultimo item da lista
+lista.pop();
+
+console.log(lista)
+
+//verificar tamanho da lista
+console.log(lista.length);
+
+//reordenar itens da lista
+console.log(lista.reverse());
+
+//retornar como String
+console.log(lista.toString());
+
+//adiciona elemento para separar os itens
+
+console.log(lista.join(" - "))
+
+
+//Dicionário
+    
+
